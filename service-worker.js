7e844b328020e3878b3659ba78153f06f49a9d39
ksp520290 <ks520290p@gmail.com>
2026-09-19 17:20:49 +0900

    ver.4.0

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "broadcast-equipment-cache-v1";
+const CORE_ASSETS = [
+  "./",
+  "./index.html",
+  "./admin.html",
+  "./history.html",
+  "./manifest.json",
+  "./data/files.json",
+  "./data/histories.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// ネットワーク優先、失敗時はキャッシュにフォールバック（閲覧画面のオフライン利用対応）
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
